ee3a8b8ad1f777cd84b471d3da394a25b26983de
Danny Sternbeck <danny.sternbeck@gmail.com>
2020-11-05 21:33:02 +0100

    Car Controller and Village Blocking added

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed, turnSpeed;
+
+    private Vector2 moveInput;
+
+    void Update()
+    {
+        moveInput.x = Input.GetAxis("Horizontal");
+        moveInput.y = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed * moveInput.y);
+        transform.Rotate(Vector3.up, turnSpeed * moveInput.x * Time.deltaTime);
+    }
+}
