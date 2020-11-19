be878a30dd15693d212388bf11d725ff764c3217
Danny Sternbeck <danny.sternbeck@gmail.com>
2020-11-19 11:42:11 +0100

    Obstacle Spawner added

---
+++ Assets/Scripts/ObstacleSpawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleSpawner : MonoBehaviour
+{
+    public GameObject wheel;
+
+    private float startDelay = 2;
+    private float spawnInterval = 1.5f;
+
+    void Start()
+    {
+
+        InvokeRepeating("spawnObstacle", startDelay, spawnInterval);
+
+
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector3.forward * 5f * Time.deltaTime);
+
+        transform.Rotate(Vector3.left * 50f * Time.deltaTime);
+    }
+
+
+    void spawnObstacle()
+    {
+        Instantiate(wheel, this.transform.position, wheel.transform.rotation);
+    }
+}
