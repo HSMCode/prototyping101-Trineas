2a097095441785b36b730cae35d3427ad5055dbf
Danny Sternbeck <danny.sternbeck@gmail.com>
2020-11-19 11:17:47 +0100

    Wheel Animator added

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
-{
+{   
     public float moveSpeed, turnSpeed;
 
     private Vector2 moveInput;
---
+++ Assets/Scripts/RotateTires.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateTires : MonoBehaviour
+{
+    private Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetAxis("Horizontal") != 0f || Input.GetAxis("Vertical") != 0f)
+        {
+            anim.SetBool("Rotate", true);
+        }
+        else
+        {
+            anim.SetBool("Rotate", false);
+        }
+    }
+}
